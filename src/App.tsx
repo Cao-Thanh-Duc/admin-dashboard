48d679cc7049e6764356bd2b9825628e262d7c21
ThanhVP6 <thanhvp6@fpt.com>
2024-12-29 20:26:04 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,108 @@
-import Home from "./pages/home/Home";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Users from "./pages/users/Users";
-import Products from "./pages/products/Products";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Menu from "./components/menu/Menu";
-import Login from "./pages/login/Login";
-import "./styles/global.scss";
-import User from "./pages/user/User";
-import Product from "./pages/product/Product";
-import Suppliers from "./pages/suppliers/Suppliers";
-import Supplier from "./pages/supplier/Supplier";
+import React from 'react';
+import Home from './pages/home/Home';
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
+import Users from './pages/users/Users';
+import Products from './pages/products/Products';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Menu from './components/menu/Menu';
+import Login from './pages/login/Login';
+import './styles/global.scss';
+import User from './pages/user/User';
+import Product from './pages/product/Product';
+import Suppliers from './pages/suppliers/Suppliers';
+import Supplier from './pages/supplier/Supplier';
 
-
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import Categorys from "./pages/categorys/Categorys";
-import Orders from "./pages/orders/Orders";
-
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Categorys from './pages/categorys/Categorys';
+import Orders from './pages/orders/Orders';
+import { AuthProvider, useAuth } from './context/useAuth';
 
 const queryClient = new QueryClient();
 
-function App() {
-  const Layout = () => {
-    return (
-      <div className="main">
-        <Navbar />
-        <div className="container">
-          <div className="menuContainer">
-            <Menu />
-          </div>
-          <div className="contentContainer">
-            <QueryClientProvider client={queryClient}>
-              <Outlet />
-            </QueryClientProvider>
-          </div>
+// Layout component
+const Layout: React.FC = () => {
+  const { isAuthenticated } = useAuth();
+
+  // Nếu chưa đăng nhập, chuyển hướng đến trang login
+  if (!isAuthenticated) {
+    return <Navigate to='/login' />;
+  }
+
+  return (
+    <div className='main'>
+      <Navbar />
+      <div className='container'>
+        <div className='menuContainer'>
+          <Menu />
         </div>
-        <Footer />
+        <div className='contentContainer'>
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+          </QueryClientProvider>
+        </div>
       </div>
-    );
-  };
+      <Footer />
+    </div>
+  );
+};
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/orders",
-          element: <Orders/>,
-        },
-        {
-          path: "/users",
-          element: <Users />,
-        },
-        {
-          path: "/suppliers",
-          element: <Suppliers />,
-        },
-        {
-          path: "/products",
-          element: <Products />,
-        },
-        {
-          path: "/categorys",
-          element: <Categorys />,
-        },
-        {
-          path: "/users/:id",
-          element: <User />,
-        },
-        {
-          path: "/supplier",
-          element: <Supplier />,
-        },
-        {
-          path: "/products/id",
-          element: <Product />,
-        },
-       
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ]);
+// Router config
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+      {
+        path: '/users',
+        element: <Users />,
+      },
+      {
+        path: '/suppliers',
+        element: <Suppliers />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+      {
+        path: '/categorys',
+        element: <Categorys />,
+      },
+      {
+        path: '/users/:id',
+        element: <User />,
+      },
+      {
+        path: '/supplier',
+        element: <Supplier />,
+      },
+      {
+        path: '/products/id',
+        element: <Product />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
-  return <RouterProvider router={router} />;
-}
+const App: React.FC = () => {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+};
 
 export default App;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,107 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { z } from 'zod';
+import { setAccessTokenToLS, setRefreshTokenToLS, setUserToLS } from '../../utils/storage';
+import './Login.css';
+import { authApi } from '../../apis/auth.api';
+import { useAuth } from '../../context/useAuth';
 
-export default Login
+const LoginSchema = z.object({
+  email: z.string().min(2, {
+    message: 'Email is valid.',
+  }),
+  password: z.string().min(6, {
+    message: 'Password must be at least 6 characters.',
+  }),
+});
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Lấy login từ context
+  const { login } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const mutationLogin = useMutation({
+    mutationKey: ['login'],
+    mutationFn: (data: any) => authApi.login(data),
+  });
+
+  const onSubmit = (data: any) => {
+    setIsLoading(true);
+    mutationLogin.mutate(data, {
+      onSuccess: (response) => {
+        setAccessTokenToLS(response.access_token);
+        setRefreshTokenToLS(response.refresh_token);
+        setUserToLS(response.user);
+
+        // Đăng nhập thành công, cập nhật trạng thái xác thực và chuyển hướng
+        login(); // Cập nhật trạng thái xác thực
+        navigate('/'); // Chuyển hướng đến trang chính
+        toast.success('Đăng nhập thành công! 🚀');
+      },
+      onError: () => {
+        toast.error('Đăng nhập thất bại!');
+      },
+      onSettled: () => {
+        setIsLoading(false);
+      },
+    });
+  };
+
+  return (
+    <div className='form-login-main'>
+      <img
+        src={'https://blog2024.theciu.vn/wp-content/uploads/2021/05/fashion.jpg'}
+        alt='bg-login'
+        className='image-login'
+      />
+      <div className='right-section'>
+        <h2 className='form-title'>Đăng nhập</h2>
+        <p className='form-login-desc'>Hãy nhập thông tin của bạn vào các ô dưới đây</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input
+              type='email'
+              placeholder='Email hoặc Số điện thoại'
+              {...register('email')}
+              required
+            />
+            {errors.email && <p className='error-message'>{errors.email.message}</p>}
+          </div>
+          <div>
+            <input type='password' placeholder='Mật khẩu' {...register('password')} required />
+            {errors.password && <p className='error-message'>{errors.password.message}</p>}
+          </div>
+          <div className='form-login-footer'>
+            <button type='submit' className='btn-login' disabled={isLoading}>
+              {isLoading ? 'Đang xử lý...' : 'Đăng nhập'}
+            </button>
+            <Link to='#' className='forgot-password'>
+              Quên mật khẩu?
+            </Link>
+          </div>
+          <Link to='/register'>
+            <p className='form-footer'>Bạn chưa có tài khoản?</p>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
